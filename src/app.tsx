5946145c10e98461fc1200ce23e7ec253b5bb493
danbolotaro <danielbolotaro13@gmail.com>
2024-02-08 15:16:51 -0300

    [FEAT] - Implementing Note Creation and Filtering

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,48 @@
+import { ChangeEvent, useState } from 'react'
 import logo from './assets/logo-nlw-expert.svg'
 import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
 
+interface Note {
+  id: string,
+  date: Date,
+  content: string
+}
+
 export function App() {
+
+  const [search, setSearch] = useState('')
+
+  const [notes, setNotes] = useState<Note[]>(() => {
+    const notesOnStorage = localStorage.getItem('notes')
+
+    if (notesOnStorage) {
+      return JSON.parse(notesOnStorage)
+    }
+
+    return []
+  })
+
+  function onNoteCreated(content: string) {
+    const newNote = {
+      id: crypto.randomUUID(),
+      date: new Date(),
+      content,
+    }
+
+    setNotes([newNote, ...notes])
+
+    localStorage.setItem('notes', JSON.stringify([newNote, ...notes]))
+  }
+
+  function handleSearch(event: ChangeEvent<HTMLInputElement>) {
+    const query = event.target.value
+
+    setSearch(query)
+  }
+
+  const filteredNotes = search !== '' ? notes.filter(note => note.content.toLowerCase().includes(search.toLowerCase())) : notes
+
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
       <img src={logo} alt="NLW Expert" />
@@ -12,6 +52,7 @@ export function App() {
           type="text"
           placeholder='Busque em suas notas...'
           className='w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500'
+          onChange={handleSearch}
         />
       </form>
 
@@ -19,17 +60,13 @@ export function App() {
 
       <div className='grid grid-cols-3 auto-rows-[250px] gap-6'>
 
-        <NewNoteCard />
+        <NewNoteCard onNoteCreated={onNoteCreated} />
 
-        <NoteCard note={{
-          date: new Date(),
-          content: 'Hello World'
-        }} />
-
-        <NoteCard note={{
-          date: new Date(2024, 2, 2),
-          content: 'Teste'
-        }} />
+        {
+          filteredNotes.map(note => {
+            return <NoteCard note={note} key={note.id} />
+          })
+        }
 
       </div>
 
