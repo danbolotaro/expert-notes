9a3ed4dde09a5f263c4d63637ea59472157ab789
danbolotaro <danielbolotaro13@gmail.com>
2024-02-08 16:39:41 -0300

    [FEAT] - Inserted SpeechRecognitionAPI and Delete Function

--- src/app.tsx
+++ src/app.tsx
@@ -35,6 +35,16 @@ export function App() {
     localStorage.setItem('notes', JSON.stringify([newNote, ...notes]))
   }
 
+  function onNoteDelete(id: string) {
+    const notesArray = notes.filter(note => {
+      return note.id !== id
+    })
+
+    setNotes(notesArray)
+
+    localStorage.setItem('notes', JSON.stringify(notesArray))
+  }
+
   function handleSearch(event: ChangeEvent<HTMLInputElement>) {
     const query = event.target.value
 
@@ -44,7 +54,7 @@ export function App() {
   const filteredNotes = search !== '' ? notes.filter(note => note.content.toLowerCase().includes(search.toLowerCase())) : notes
 
   return (
-    <div className="mx-auto max-w-6xl my-12 space-y-6">
+    <div className="mx-auto max-w-6xl my-12 space-y-6 px-5">
       <img src={logo} alt="NLW Expert" />
 
       <form className='w-full'>
@@ -58,13 +68,13 @@ export function App() {
 
       <div className='h-px bg-slate-700' />
 
-      <div className='grid grid-cols-3 auto-rows-[250px] gap-6'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 auto-rows-[250px] gap-6'>
 
         <NewNoteCard onNoteCreated={onNoteCreated} />
 
         {
           filteredNotes.map(note => {
-            return <NoteCard note={note} key={note.id} />
+            return <NoteCard note={note} key={note.id} onNoteDelete={onNoteDelete} />
           })
         }
 
